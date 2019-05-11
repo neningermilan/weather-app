2ca6dcf26e8e835b487726de44317f213ee9c1e6
neningermilan <neninger.milan@gmail.com>
2019-05-11 15:50:02 +0200

    A táblázat kiegészítése egyéb időjárással kapcsolatos adatokkal.

--- src/Components/WeatherBox.js
+++ src/Components/WeatherBox.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 
 let WeatherBox = ({ weather, isLoading, search }) => {
     let weatherItem = '';
-
     isLoading ?
         weatherItem = <div className="spinner-border centered" role="status">
             <span class="sr-only">Loading...</span>
@@ -12,12 +11,20 @@ let WeatherBox = ({ weather, isLoading, search }) => {
             weatherItem = weather.map((weather, index) => (
                 <tr key={`${index}`} >
                     <td>{weather.name}</td>
+                    <td>{weather.main.temp} ℃</td>
+                    <td>{weather.main.temp_min} ℃</td>
+                    <td>{weather.main.temp_max} ℃</td>
+                    <td>{weather.main.humidity} %</td>
                 </tr>
             )
             ) :
             weatherItem = search.map((search, index) => (
                 <tr key={`${index}`}>
                     <td>{search.name}</td>
+                    <td>{search.main.temp} ℃</td>
+                    <td>{search.main.temp_min} ℃</td>
+                    <td>{search.main.temp_max} ℃</td>
+                    <td>{search.main.humidity} %</td>
                 </tr>
             )
             )
@@ -27,6 +34,10 @@ let WeatherBox = ({ weather, isLoading, search }) => {
             <thead>
                 <tr>
                     <th scope="col">City</th>
+                    <th scope="col">Temperature</th>
+                    <th scope="col">Min. Temperature</th>
+                    <th scope="col">Max. Temperature</th>
+                    <th scope="col">Humidity</th>
                 </tr>
             </thead>
             <tbody>
@@ -43,34 +54,3 @@ const mapStateToProps = (state) => (state);
 WeatherBox = connect(mapStateToProps, null)(WeatherBox)
 
 export default WeatherBox;
-
-/* <table class="table table-hover table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-  </table>
- */
