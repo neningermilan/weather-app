cf5aae6d237a3a2eacad83e5121fb8b402dccf03
neningermilan <neninger.milan@gmail.com>
2019-05-12 20:28:16 +0200

    Updates

---
+++ precache-manifest.8508f63e36d52bf23602a2358b2d95c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83de818d2dc7250e8c3d26f34925b2d4",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "b881252c77400ca98633",
+    "url": "/weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c8f71cd4457abdfb4ab4",
+    "url": "/weather-app/static/css/main.d6cb2d0b.chunk.css"
+  },
+  {
+    "revision": "b881252c77400ca98633",
+    "url": "/weather-app/static/js/2.87962721.chunk.js"
+  },
+  {
+    "revision": "c8f71cd4457abdfb4ab4",
+    "url": "/weather-app/static/js/main.33905473.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
